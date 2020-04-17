13e4070b25f93e09ef90f070332a18cf3ab0a01b
Sydney Faux <48231621+sfaux@users.noreply.github.com>
2020-04-17 19:39:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
+npm install wolfram-notebook-embedder
+import WolframNotebookEmbedder from 'wolfram-notebook-embedder';
 WolframNotebookEmbedder.embed('https://www.wolframcloud.com/obj/myusername/myfolder/mynotebook.nb', document.getElementById('notebookContainer'));
