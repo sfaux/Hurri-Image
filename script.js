e2c30b5888b4983e5f60df0825828047d603ad13
Sydney Faux <48231621+sfaux@users.noreply.github.com>
2020-04-17 20:32:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 npm install wolfram-notebook-embedder
 import WolframNotebookEmbedder from 'wolfram-notebook-embedder';
 WolframNotebookEmbedder.embed('https://www.wolframcloud.com/obj/myusername/myfolder/mynotebook.nb', document.getElementById('notebookContainer'));
+embedding.then(function (nb) {
+    // This will reset the DynamicModule variable x$$
+    // in the first cell of the notebook.
+    return nb.getCells().then(function (cells) {
+        nb.setDynamicModuleVariable({
+            cellId: cells[0].id,
+            name: 'x$$',
+            value: 0
+        });
+    });
+})
